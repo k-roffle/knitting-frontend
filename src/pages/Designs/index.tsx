8ab35714fb03d6bc68bf63c138a8f0165a14a6f5
Yuurilee <yurilee9696@gmail.com>
2021-03-12 00:07:17 +0900

    Feat: 초기 라우터 세팅
    - Error404 페이지
    - CreateDesign 페이지
    - CreateDesign 하위 컴포넌트 Detail, Pattern, Review
    - Designs 라우터를 설정한 index
    
    resolve #3

---
+++ src/pages/Designs/index.tsx
@@ -0,0 +1,17 @@
+import { Error404 } from 'pages';
+import CreateDesign from 'pages/CreateDesign';
+import React from 'react';
+import { Switch, Route, RouteComponentProps } from 'react-router-dom';
+
+const Designs = ({
+  match: { url },
+}: RouteComponentProps): React.ReactElement => {
+  return (
+    <Switch>
+      <Route path={`${url}/create`} component={CreateDesign} exact />
+      <Route path="*" component={Error404} />
+    </Switch>
+  );
+};
+
+export default Designs;
