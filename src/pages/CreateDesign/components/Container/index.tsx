ebf5ffc9ae811d6be2f4e9cca0ef5ea4bb0f8ecd
kimdoori <dooit0228@gmail.com>
2021-03-14 14:48:35 +0900

    Feat: Container 추가

---
+++ src/pages/CreateDesign/components/Container/index.tsx
@@ -0,0 +1,38 @@
+import { Detail } from 'pages';
+import Pattern from 'pages/CreateDesign/Pattern';
+import Review from 'pages/CreateDesign/Review';
+import { currentStepAtom } from 'pages/CreateDesign/recoils';
+import { PAGE } from 'pages/CreateDesign/types';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+
+import Footer from '../Footer';
+import Header from '../Header';
+
+const Container = (): React.ReactElement => {
+  const currentStep = useRecoilValue(currentStepAtom);
+
+  const renderContent = (): React.ReactElement => {
+    switch (currentStep) {
+      case PAGE.DETAIL:
+        return <Detail />;
+      case PAGE.PATTERN:
+        return <Pattern />;
+      case PAGE.REVIEW:
+        return <Review />;
+      default:
+        return <Detail />;
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <Detail />
+      {renderContent}
+      <Footer />
+    </div>
+  );
+};
+
+export default Container;
