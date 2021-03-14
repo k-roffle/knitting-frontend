1686626b4e773361ad074c83e11845674f543397
kimdoori <dooit0228@gmail.com>
2021-03-14 14:47:04 +0900

    Feat: Header 추가

---
+++ src/pages/CreateDesign/components/Header/index.tsx
@@ -0,0 +1,41 @@
+import { Grid, Typography } from '@material-ui/core';
+import { currentStepAtom } from 'pages/CreateDesign/recoils';
+import { PAGE } from 'pages/CreateDesign/types';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+const Contents = styled(Typography)`
+  white-space: pre-line;
+`;
+
+const Header = (): React.ReactElement => {
+  const currentStep = useRecoilValue(currentStepAtom);
+
+  const renderContents = (): string => {
+    switch (currentStep) {
+      case PAGE.DETAIL:
+        return '도안의 기본 정보를 입력하여 주세요.\n니터들이 원하는 도안을 더 빠르게 찾을 수 있을 거예요!';
+      case PAGE.PATTERN:
+        return '편물을 만드는 방법을 단계별로 작성해주세요.\n게이지 계산이 필요한 코와 단의 경우 자동완성 기능을 사용하면,\n니팅이 게이지 계산을 자동으로 도와드릴게요! :D';
+      case PAGE.REVIEW:
+        return '입력한 도안이 올바른지 확인해보세요!\n정확한 도안은 니터로부터 더 높은 만족을 받을 수 있을 거예요;)';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={8}>
+        <Typography variant="h6">🧶 새로운 도안 작성하기</Typography>
+        <Contents>{renderContents()}</Contents>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <div>프로그래스바 들어갈 자리</div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Header;
