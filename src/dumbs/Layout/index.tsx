42191a3e312fa020c34a9bc7cd538562055e82a9
Yuurilee <yurilee9696@gmail.com>
2021-03-16 23:18:20 +0900

    Feat: 페이지에서 공통으로 사용할 Layout 컴포넌트 추가

---
+++ src/dumbs/Layout/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Content = styled.section`
+  max-width: 1100px;
+  margin: 36px auto;
+`;
+
+const Layout = ({ children }: Props): React.ReactElement => {
+  return (
+    <>
+      <Content>{children}</Content>
+    </>
+  );
+};
+
+export default Layout;
