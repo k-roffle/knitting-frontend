c7021225325546141b4135197f80cbc867828da5
Yuurilee <yurilee9696@gmail.com>
2021-04-03 22:49:00 +0900

    Feat: Pattern component에 Editor component 추가

--- src/pages/CreateDesign/Pattern/index.tsx
+++ src/pages/CreateDesign/Pattern/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Editor from '@draft-js-plugins/editor';
+import { EditorState } from 'draft-js';
+import { useRef, useState } from 'react';
 
 const Pattern = (): React.ReactElement => {
-  return <div>도안 작성 페이지</div>;
+  const [editorState, setEditorState] = useState<EditorState>(
+    EditorState.createEmpty(),
+  );
+  const editor = useRef<Editor | null>(null);
+
+  const focusEditor = (): void => {
+    editor?.current?.focus();
+  };
+
+  return (
+    <div>
+      <div onClick={focusEditor}>
+        <Editor
+          ref={editor}
+          editorState={editorState}
+          onChange={setEditorState}
+          placeholder="도안을 입력하세요"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Pattern;
