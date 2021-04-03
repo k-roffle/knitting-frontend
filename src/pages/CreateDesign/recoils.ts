3158af736351bae068c10be9f3d572c59f751e56
kimdoori <dooit0228@gmail.com>
2021-04-03 12:06:08 +0900

    Add: currentDetailInputsAtom atom

--- src/pages/CreateDesign/recoils.ts
+++ src/pages/CreateDesign/recoils.ts
@@ -1,8 +1,29 @@
 import { atom } from 'recoil';
 
-import { PAGE, PAGE_TYPE } from './types';
+import { DESIGN, DetailInputs, PAGE, PAGE_TYPE, PATTERN } from './types';
 
 export const currentStepAtom = atom<PAGE_TYPE>({
   key: 'currentStep',
   default: PAGE.DETAIL,
+});
+
+export const currentDetailInputsAtom = atom<DetailInputs>({
+  key: 'currentDetailInputs',
+  default: {
+    name: '',
+    designType: DESIGN.SWEATER,
+    patternType: PATTERN.TEXT,
+    stitches: 0,
+    rows: 0,
+    totalLength: 0,
+    retailLength: 0,
+    shoulderLength: 0,
+    bottomLength: 0,
+    armLength: 0,
+    needle: '',
+    yarn: null,
+    extra: null,
+    price: 0,
+    pattern: '',
+  },
 });
