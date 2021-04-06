a6be07230bb93ebc06ac071cb5b419a50136d1b2
Yuurilee <yurilee9696@gmail.com>
2021-04-06 23:24:49 +0900

    Refactor: unitDecoratorRegex - eslint 수정

--- src/plugins/unitDecorator/utils/unitDecoratorRegex.ts
+++ src/plugins/unitDecorator/utils/unitDecoratorRegex.ts
@@ -1,12 +1,12 @@
-/* eslint-disable */
 const numbers = '0-9';
 
-export const getEndHashtagMatch = (unit: string) =>
+export const getEndHashtagMatch = (unit: string): RegExp =>
+  // eslint-disable-next-line no-useless-escape
   new RegExp(`^(?:[${numbers}]+[${unit}]|:\/\/)`);
 
-export const getHashSigns = (unit: string) => new RegExp(unit);
+export const getHashSigns = (unit: string): RegExp => new RegExp(unit);
 
-export const getUnitDecoratorBoundary = (unit: string) =>
+export const getUnitDecoratorBoundary = (unit: string): RegExp =>
   new RegExp(
     `((?:^|$|[^${numbers}]))([${numbers}]*)([${numbers}]+[${unit}])`,
     'gi',
