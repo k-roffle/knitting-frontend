1358fd5f217cdb0126f6d419595cad0977336362
Yuurilee <yurilee9696@gmail.com>
2021-04-18 01:27:25 +0900

    Feat: react-draft-wysiwyg를 참조하여 FontSize 컴포넌트 추가
    - https://github.com/jpuri/react-draft-wysiwyg/tree/master/src/controls/FontSize

---
+++ src/pages/CreateDesign/components/FontSize/index.tsx
@@ -0,0 +1,60 @@
+import { toggleCustomInlineStyle } from 'pages/libs/draftjs-utils/inline';
+import React, { useEffect, useState } from 'react';
+
+import { DropDown } from '../DropDown';
+import { DropdownOption } from '../DropDown/DropDownOption';
+
+import { fontSizeOptions, FontSizeProps } from './types';
+
+export const FontSize = ({
+  onChange,
+  onChageCustomStyleMap,
+  editorState,
+  defaultFontSize,
+  fontSize,
+}: FontSizeProps): React.ReactElement => {
+  const toggleFontSize = (newFontSize: number) => {
+    const newState = toggleCustomInlineStyle(
+      editorState,
+      'fontSize',
+      newFontSize,
+      newFontSize === currentFontSize,
+      onChageCustomStyleMap,
+    );
+
+    if (newState) {
+      onChange?.(newState);
+    }
+  };
+
+  const [currentFontSize, setCurrentFontSize] = useState(
+    fontSize || defaultFontSize,
+  );
+
+  useEffect(() => {
+    setCurrentFontSize(fontSize || defaultFontSize);
+  }, [fontSize]);
+
+  return (
+    <DropDown
+      value={currentFontSize}
+      onChange={(size) => {
+        setCurrentFontSize(size === currentFontSize ? defaultFontSize : size);
+        toggleFontSize(size);
+      }}
+    >
+      {currentFontSize && <span>{currentFontSize}</span>}
+      {fontSizeOptions.map((size, index) => {
+        return (
+          <DropdownOption
+            active={currentFontSize === size}
+            value={size}
+            key={index.toString()}
+          >
+            {size}
+          </DropdownOption>
+        );
+      })}
+    </DropDown>
+  );
+};
---
+++ src/pages/CreateDesign/components/FontSize/types.ts
@@ -0,0 +1,26 @@
+import { DraftStyleMap, EditorState } from 'draft-js';
+
+export type FontSizeProps = {
+  onChange?: (editorState: EditorState) => void;
+  onChageCustomStyleMap: (customStyleMap: DraftStyleMap) => void;
+
+  editorState: EditorState;
+  defaultFontSize: number;
+  fontSize?: number;
+};
+
+export const fontSizeOptions = [
+  8,
+  10,
+  12,
+  14,
+  16,
+  18,
+  24,
+  30,
+  36,
+  48,
+  60,
+  72,
+  96,
+];
