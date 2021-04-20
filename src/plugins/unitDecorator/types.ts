fb1a81febe785645654d355f43a99a6c702bd458
Yuurilee <yurilee9696@gmail.com>
2021-04-21 01:18:24 +0900

    Feat: 계산할 코와 단도 inlineStyleRanges로 offset 알 수 있도록 수정

--- src/plugins/unitDecorator/types.ts
+++ src/plugins/unitDecorator/types.ts
@@ -1,9 +1,5 @@
 import { palette } from 'themes/palatte';
 
-export const CustomInline = {
-  NOT_CALCULATE: 'NOT_CALCULATE',
-};
-
 export const UnitDecoratorStyleMap = {
   NOT_CALCULATE: {
     background: palette.action.disabledBackground,
