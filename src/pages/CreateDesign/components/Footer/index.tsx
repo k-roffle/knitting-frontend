36314de48ceebae9c28b1f1ebc301faee7a899ab
kimdoori <dooit0228@gmail.com>
2021-04-24 17:52:23 +0900

    Impl: 서버에 저장하도록

--- src/pages/CreateDesign/components/Footer/index.tsx
+++ src/pages/CreateDesign/components/Footer/index.tsx
@@ -1,14 +1,78 @@
 import Button, { SIDE } from 'dumbs/Button';
-import { currentStepAtom } from 'pages/CreateDesign/recoils';
+import {
+  currentDesignInputAtom,
+  currentStepAtom,
+} from 'pages/CreateDesign/recoils';
 import { PAGE } from 'pages/CreateDesign/types';
 import React from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+
+import { request } from '../../../../utils/requests';
 
 const Footer = (): React.ReactElement => {
   const [currentStep, setCurrentStep] = useRecoilState(currentStepAtom);
+  const {
+    name,
+    stitches,
+    rows,
+    totalLength,
+    sleeveLength,
+    shoulderWidth,
+    bottomWidth,
+    armholeDepth,
+    needle,
+    yarn,
+    extra,
+    price,
+    designType,
+    patternType,
+  } = useRecoilValue(currentDesignInputAtom);
+
+  const serializeSize = (value: number): Record<string, string | number> => {
+    return {
+      value,
+      unit: 'Cm',
+    };
+  };
 
   const saveDesign = async (): Promise<void> => {
-    // TODO: 저장 기능 구현
+    const createdAt = new Date();
+
+    try {
+      await request('/design/', 'post', {
+        id: uuidv4(),
+        name,
+        gauge: {
+          stitches,
+          rows,
+        },
+        size: {
+          totalLength: serializeSize(totalLength),
+          sleeveLength: serializeSize(sleeveLength),
+          shoulderWidth: serializeSize(shoulderWidth),
+          bottomWidth: serializeSize(bottomWidth),
+          armholeDepth: serializeSize(armholeDepth),
+        },
+        needle,
+        yarn,
+        extra,
+        price: {
+          value: price,
+        },
+        designType,
+        patternType,
+        pattern: {
+          value: '',
+        },
+        createdAt: createdAt.toISOString(),
+      });
+
+      window.location.reload();
+    } catch (e) {
+      // eslint-disable-next-line no-alert
+      alert('도안 저장에 실패했습니다.');
+    }
   };
 
   const handleOnClickPrevious = (): void => {
