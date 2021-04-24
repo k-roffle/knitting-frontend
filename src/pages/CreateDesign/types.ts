e1f40b7061963387462b17e821fd9bfd79c4ec06
kimdoori <dooit0228@gmail.com>
2021-04-24 17:50:44 +0900

    Modify: 파스칼 케이스로

--- src/pages/CreateDesign/types.ts
+++ src/pages/CreateDesign/types.ts
@@ -7,15 +7,15 @@ export const PAGE = {
 export type PAGE_TYPE = typeof PAGE[keyof typeof PAGE];
 
 export const DESIGN = {
-  SWEATER: 'SWEATER',
+  SWEATER: 'Sweater',
 } as const;
 
 export type DESIGN_TYPE = typeof DESIGN[keyof typeof DESIGN];
 
 export const PATTERN = {
-  TEXT: 'TEXT',
-  IMAGE: 'IMAGE',
-  VIDEO: 'VIDEO',
+  TEXT: 'Text',
+  IMAGE: 'Image',
+  VIDEO: 'Video',
 } as const;
 
 export type PATTERN_TYPE = typeof PATTERN[keyof typeof PATTERN];
