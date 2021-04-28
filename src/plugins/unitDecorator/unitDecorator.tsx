1bf68a152d48570720a8cccaaf25fa8d2bc758cb
Yuurilee <yurilee9696@gmail.com>
2021-04-28 23:54:40 +0900

    Fix: 에디터 여러 줄 입력 했을 경우 끝에 글자 사라지는  문제 수정

--- src/plugins/unitDecorator/unitDecorator.tsx
+++ src/plugins/unitDecorator/unitDecorator.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ContentState, convertToRaw, EditorState, RichUtils } from 'draft-js';
+import { ContentState, EditorState } from 'draft-js';
 import { changeOriginalStyleToNeweStyle } from 'pages/libs/draftjs-utils/inline';
 import { ReactElement, ReactNode, useEffect } from 'react';
 import styled from 'styled-components';
@@ -14,12 +14,12 @@ export interface UnitDecoratorProps {
   decoratedText?: string;
   entityKey?: string;
   offsetKey?: string;
-  contentState?: ContentState;
+  contentState: ContentState;
   blockKey?: string;
   start?: number;
   end?: number;
 
-  setEditorState?(editorState: EditorState): void;
+  setEditorState(editorState: EditorState): void;
   getEditorState(): EditorState;
 }
 
@@ -56,52 +56,52 @@ export default function UnitDecorator(props: UnitDecoratorProps): ReactElement {
     ...otherProps
   } = props;
 
-  const editorState = getEditorState?.();
+  const editorState = getEditorState();
   const calculateKey =
     unit === '코' ? 'STITCH_CALCULATE_ROUND' : 'ROW_CALCULATE_ROUND';
 
-  const selectionState = editorState.getSelection();
-  const newSelection = selectionState.merge({
-    anchorOffset: start,
-    focusOffset: end,
-  });
-
-  const editorStateWithNewSelection = EditorState.forceSelection(
-    editorState,
-    newSelection,
-  );
-
-  const currentStyle = editorStateWithNewSelection.getCurrentInlineStyle();
-  const canCalculate =
-    !currentStyle.has('NOT_CALCULATE') && !currentStyle.has(calculateKey);
-
   useEffect(() => {
-    if (setEditorState != null) {
-      if (canCalculate) {
-        const newEeditorState = changeOriginalStyleToNeweStyle({
-          editorState,
-          originalStyle: calculateKey,
-          startOffset: start,
-          endOffset: end,
-        });
+    const selectionState = editorState.getSelection();
+    const newSelection = selectionState.merge({
+      anchorKey: blockKey,
+      focusKey: blockKey,
+      anchorOffset: start,
+      focusOffset: end,
+    });
 
-        setEditorState(newEeditorState);
-      }
-    }
-  }, [canCalculate]);
+    const editorStateWithDecoratorSelection = EditorState.forceSelection(
+      editorState,
+      newSelection,
+    );
 
-  const handleClick = (): void => {
-    if (editorState != null && children != null && setEditorState != null) {
+    const decoratorStyle = editorStateWithDecoratorSelection.getCurrentInlineStyle();
+    const canCalculate =
+      !decoratorStyle.has('NOT_CALCULATE') && !decoratorStyle.has(calculateKey);
+
+    if (canCalculate) {
       const newEeditorState = changeOriginalStyleToNeweStyle({
         editorState,
+        blockKey,
         originalStyle: calculateKey,
-        newStyle: 'NOT_CALCULATE',
         startOffset: start,
         endOffset: end,
       });
 
       setEditorState(newEeditorState);
     }
+  }, [editorState]);
+
+  const handleClick = (): void => {
+    const newEeditorState = changeOriginalStyleToNeweStyle({
+      editorState: getEditorState(),
+      blockKey,
+      originalStyle: calculateKey,
+      newStyle: 'NOT_CALCULATE',
+      startOffset: start,
+      endOffset: end,
+    });
+
+    setEditorState(newEeditorState);
   };
 
   return (
