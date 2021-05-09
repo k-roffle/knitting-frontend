c0f75f31fdfa18520ba9e391f84095a43924b78c
Yuurilee <yurilee9696@gmail.com>
2021-05-09 19:45:07 +0900

    Design: palette secondary color 오버라이딩

--- src/themes/palatte.ts
+++ src/themes/palatte.ts
@@ -1,4 +1,5 @@
 import { createMuiTheme } from '@material-ui/core';
+import blue from '@material-ui/core/colors/blue';
 
 import { deepOrange } from './colors/deepOrange';
 
@@ -8,6 +9,11 @@ export const { palette } = createMuiTheme({
       light: deepOrange[200],
       main: deepOrange[600],
       dark: deepOrange[800],
+    },
+    secondary: {
+      light: blue[200],
+      main: blue[600],
+      dark: blue[800],
     },
     action: {
       hover: 'rgba(0, 0, 0, 0.06)',
