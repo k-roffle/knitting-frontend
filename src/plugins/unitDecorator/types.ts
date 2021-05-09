52ccc4bcb687f6fb6d3e36c06c368731a11b0ced
Yuurilee <yurilee9696@gmail.com>
2021-05-09 19:04:55 +0900

    Feat: 코 올림, 내림 키 추가 및 스타일 추가

--- src/plugins/unitDecorator/types.ts
+++ src/plugins/unitDecorator/types.ts
@@ -1,8 +1,17 @@
+import { deepOrange } from 'themes/colors/deepOrange';
 import { palette } from 'themes/palatte';
 
 export const UnitDecoratorStyleMap = {
   NOT_CALCULATE: {
     background: palette.action.disabledBackground,
     color: palette.text.primary,
+  },
+  STITCH_CALCULATE_ROUND_UP: {
+    background: deepOrange[900],
+    color: palette.background.paper,
+  },
+  STITCH_CALCULATE_ROUND_DOWN: {
+    background: deepOrange[300],
+    color: palette.background.paper,
   },
 };
