9fe1883857325ff419e4a5b9f63924224463e810
kimdoori <dooit0228@gmail.com>
2021-05-15 13:58:37 +0900

    Add: Snackbar

---
+++ src/dumbs/Snackbar/index.tsx
@@ -0,0 +1,38 @@
+import { Snackbar as MaterialSnackbar } from '@material-ui/core';
+import MuiAlert, { Color } from '@material-ui/lab/Alert';
+import React from 'react';
+
+interface Props {
+  autoHideDuration: number;
+  label: string;
+  onClose?(): void;
+  open: boolean;
+  severity: Color;
+}
+
+const Snackbar = ({
+  autoHideDuration,
+  label,
+  onClose,
+  open,
+  severity,
+}: Props): React.ReactElement => {
+  return (
+    <MaterialSnackbar
+      open={open}
+      autoHideDuration={autoHideDuration}
+      onClose={onClose}
+    >
+      <MuiAlert
+        elevation={6}
+        variant="filled"
+        severity={severity}
+        onClose={onClose}
+      >
+        {label}
+      </MuiAlert>
+    </MaterialSnackbar>
+  );
+};
+
+export default Snackbar;
