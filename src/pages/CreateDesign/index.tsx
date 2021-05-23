784a0603ef696ee3388695bcade2a6c1cb2df7ac
kimdoori <dooit0228@gmail.com>
2021-05-23 12:29:00 +0900

    Modify: ThemeProvider 위치

--- src/pages/CreateDesign/index.tsx
+++ src/pages/CreateDesign/index.tsx
@@ -1,16 +1,12 @@
-import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { theme } from 'themes';
 
 import Container from './components/Container';
 
 const CreateDesign = (): React.ReactElement => {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Container />
-      </ThemeProvider>
+      <Container />
     </RecoilRoot>
   );
 };
