01bb5f0631529e64a2303fcf830fb86f6a303225
Yuurilee <yurilee9696@gmail.com>
2021-05-26 00:11:33 +0900

    Feat: 각 unit 타입별로 타입에 맞는 결과를 리턴해주는 함수들 추가

---
+++ src/plugins/unitDecorator/utils.ts
@@ -0,0 +1,53 @@
+import {
+  STITCH_DISPLAY_APPROXIMATION,
+  ROW_DISPLAY_APPROXIMATION,
+  REPEAT_APPROXIMATION,
+  REPEAT_DISPLAY_APPROXIMATION,
+  UNIT_TYPE,
+  UNIT_APPROXIMATION_TYPE,
+  ROW_APPROXIMATION,
+  STITCH_APPROXIMATION,
+} from './types';
+
+export const getCalculateKey = (unit?: UNIT_TYPE): string => {
+  switch (unit) {
+    case '코':
+      return 'STITCH_CALCULATE';
+    case '단':
+      return 'ROW_CALCULATE';
+    case '번':
+      return 'REPEAT_CALCULATE';
+    default:
+      return '';
+  }
+};
+
+export const getOriginalStyle = (
+  unit: UNIT_TYPE | undefined,
+): UNIT_APPROXIMATION_TYPE => {
+  switch (unit) {
+    case '코':
+      return STITCH_APPROXIMATION.STITCH_CALCULATE_ROUND;
+    case '단':
+      return ROW_APPROXIMATION.ROW_CALCULATE_ROUND;
+    case '번':
+      return REPEAT_APPROXIMATION.STITCH_REPEAT_CALCULATE_ROUND;
+    default:
+      return STITCH_APPROXIMATION.STITCH_CALCULATE_ROUND;
+  }
+};
+
+export const getDisplayedApproximations = (
+  unit?: UNIT_TYPE,
+): Record<string, string> => {
+  switch (unit) {
+    case '코':
+      return STITCH_DISPLAY_APPROXIMATION;
+    case '단':
+      return ROW_DISPLAY_APPROXIMATION;
+    case '번':
+      return REPEAT_DISPLAY_APPROXIMATION;
+    default:
+      return {};
+  }
+};
