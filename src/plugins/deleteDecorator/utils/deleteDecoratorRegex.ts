d7f446813128290285743a8dcfe62f10b0410a82
Yuurilee <yurilee9696@gmail.com>
2021-05-26 00:36:43 +0900

    Rename: 오타 수정

--- src/plugins/deleteDecorator/utils/deleteDecoratorRegex.ts
+++ src/plugins/deleteDecorator/utils/deleteDecoratorRegex.ts
@@ -3,6 +3,6 @@ import { numbers } from 'plugins/unitDecorator/utils/unitDecoratorRegex';
 export const getUnitTotallyMatch = (units = '코'): RegExp =>
   new RegExp(`^[${numbers}]+[${units}]`, 'gi');
 
-export const getAllGroupesIntoSpace = (): RegExp =>
+export const getAllGroupsIntoSpace = (): RegExp =>
   // eslint-disable-next-line no-useless-escape
   new RegExp(`(\\S{1,})`, 'g');
