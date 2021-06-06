ede9ad5b5b108e19886a3ff16d608b6e610a69b9
doori.kim <doori.kim@toss.im>
2021-06-06 12:03:32 +0900

    Impl: 구글 로그인 버튼 기능

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
 import { Button as MaterialButton } from '@material-ui/core';
 import Background from 'assets/designs/background_login.png';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { defaultShadow, flexCenterAlign } from 'styles/constants';
 import { theme } from 'themes';
+import { request } from 'utils/requests';
 
 const MainWrapper = styled.div`
   background-image: url(${Background});
@@ -35,10 +37,24 @@ const LoginButton = styled(MaterialButton)`
 `;
 
 const Login = (): React.ReactElement => {
+  const history = useHistory();
+
+  const handleOnClickLogin = async (): Promise<void> => {
+    try {
+      await request('/auth/google/code', 'post');
+    } catch (e) {
+      history.push('/error/');
+    }
+  };
+
   return (
     <MainWrapper>
       <LoginContainer>
-        <LoginButton color="primary" variant="contained">
+        <LoginButton
+          color="primary"
+          variant="contained"
+          onClick={handleOnClickLogin}
+        >
           Google 계정으로 계속하기
         </LoginButton>
       </LoginContainer>
