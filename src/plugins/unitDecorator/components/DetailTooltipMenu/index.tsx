4a9d4b9a00bc6302de639ccb64af3bbf2b0b2cea
Yuurilee <yurilee9696@gmail.com>
2021-06-12 19:53:49 +0900

    Refactor: 툴팁 메뉴 렌더링 각각 컴포넌트로 분리

---
+++ src/plugins/unitDecorator/components/DetailTooltipMenu/index.tsx
@@ -0,0 +1,65 @@
+import { UNIT_APPROXIMATION_TYPE } from 'plugins/unitDecorator/types';
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { theme } from 'themes';
+import { palette } from 'themes/palatte';
+
+export interface TooltipMenuProps {
+  isSelectedCalculateKey: boolean;
+}
+
+export const TooltipMenuContainer = styled.div`
+  display: flex;
+`;
+
+export const TooltipMenu = styled.span<TooltipMenuProps>`
+  padding: ${theme.spacing(0.8)};
+  margin: ${theme.spacing(0.2)};
+  white-space: nowrap;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${palette.action.hover};
+  }
+
+  ${({ isSelectedCalculateKey }) =>
+    isSelectedCalculateKey &&
+    css`
+      background-color: ${palette.action.selected};
+    `}
+`;
+
+interface Props {
+  currentCalculateKey: UNIT_APPROXIMATION_TYPE | undefined;
+  displayedApproximations: Record<string, string>;
+
+  onClick(key: UNIT_APPROXIMATION_TYPE): void;
+}
+
+export const DetailTooltipMenu = ({
+  currentCalculateKey,
+  displayedApproximations,
+  onClick,
+}: Props): React.ReactElement => (
+  <TooltipMenuContainer>
+    {Object.values(displayedApproximations).map(
+      (displayApproximatio, index): React.ReactElement => {
+        const menuKey = Object.keys(displayedApproximations)[
+          index
+        ] as UNIT_APPROXIMATION_TYPE;
+
+        const isSelectedCalculateKey = currentCalculateKey === menuKey;
+
+        return (
+          <TooltipMenu
+            key={menuKey}
+            onClick={(): void => onClick(menuKey)}
+            isSelectedCalculateKey={isSelectedCalculateKey}
+          >
+            {displayApproximatio}
+          </TooltipMenu>
+        );
+      },
+    )}
+  </TooltipMenuContainer>
+);
