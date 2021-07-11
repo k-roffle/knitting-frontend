aecc0860b4c69f12734f53af5cb673d8300f8056
yool <devuri404@gmail.com>
2021-07-11 10:19:46 +0900

    feat: make CreateDesgins router private

--- src/routers/DesignRouter/index.tsx
+++ src/routers/DesignRouter/index.tsx
@@ -2,13 +2,14 @@ import CreateDesign from 'pages/CreateDesign';
 import Error404 from 'pages/Error404';
 import React from 'react';
 import { Switch, Route, RouteComponentProps } from 'react-router-dom';
+import { ProtectedRoute } from 'utils/route';
 
 const DesignRouter = ({
   match: { url },
 }: RouteComponentProps): React.ReactElement => {
   return (
     <Switch>
-      <Route path={`${url}/create/`} component={CreateDesign} exact />
+      <ProtectedRoute path={`${url}/create/`} component={CreateDesign} exact />
       <Route path="*" component={Error404} />
     </Switch>
   );
