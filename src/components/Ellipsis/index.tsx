4364fdb5601244e74f1569e04a496300624a44b0
Yuurilee <yurilee9696@gmail.com>
2021-08-15 16:49:13 +0900

    Feat: Ellipsis 컴포넌트 추가

---
+++ src/components/Ellipsis/index.tsx
@@ -0,0 +1,60 @@
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import OriginalEllipsis from 'react-ellipsis-component';
+import styled from 'styled-components';
+
+const EllipsisWrapper = styled.div`
+  overflow: hidden;
+`;
+
+export interface JsEllipsisProps {
+  text: string;
+  dangerouslyUseInnerHTML?: boolean;
+  maxLine?: number;
+  endExcludes?: string[];
+  reflowThresholdOnResize?: number;
+  onReflow?: (ellipsis: boolean, text: string) => void;
+  onEllipsisClick?: () => void;
+}
+
+const Ellipsis = ({
+  maxLine = 1,
+  ...other
+}: JsEllipsisProps): React.ReactElement => {
+  const ellipsisRef = useRef<HTMLDivElement>(null);
+  const [maxHeight, setMaxHeight] = useState(0);
+
+  useLayoutEffect(() => {
+    const tooltipTarget = ellipsisRef?.current;
+    const lineHeight =
+      tooltipTarget == null
+        ? null
+        : window.getComputedStyle(tooltipTarget).lineHeight;
+
+    const lineHeightNumber = Number(lineHeight?.replace(/[^-\\.0-9]/g, ''));
+    const newMaxHeight = lineHeightNumber * maxLine + lineHeightNumber / 2;
+
+    setMaxHeight(Math.round(newMaxHeight));
+  }, [ellipsisRef, maxLine]);
+
+  return (
+    <EllipsisWrapper
+      ref={ellipsisRef}
+      style={{
+        maxHeight: `${maxHeight}px`,
+      }}
+    >
+      {maxHeight !== 0 && (
+        <OriginalEllipsis
+          maxLine={maxLine}
+          maxHeight={maxHeight}
+          ellipsis
+          reflowOnResize
+          ellipsisNode="..."
+          {...other}
+        />
+      )}
+    </EllipsisWrapper>
+  );
+};
+
+export default Ellipsis;
