7ae2a35e777ed8f96a47afe596881434243a63bb
Yuurilee <yurilee9696@gmail.com>
2021-08-18 23:56:08 +0900

    Feat: CommonSnackbar와 snackbar를 띄우는 훅 추가

---
+++ src/components/CommonSnackbar/index.tsx
@@ -0,0 +1,28 @@
+import { Color } from '@material-ui/lab/Alert';
+import { Snackbar } from 'dumbs';
+import React from 'react';
+import { atom, useRecoilValue } from 'recoil';
+
+export type SnackbarParams = {
+  message: string;
+  severity: Color;
+};
+
+export const snackbarAtom = atom<SnackbarParams | undefined>({
+  key: 'snackbar',
+  default: undefined,
+});
+
+const CommonSnackbar = (): React.ReactElement | null => {
+  const { message, severity } = useRecoilValue(snackbarAtom) ?? {};
+
+  if (message == null) {
+    return null;
+  }
+
+  return (
+    <Snackbar label={message} open={true} severity={severity ?? 'error'} />
+  );
+};
+
+export default CommonSnackbar;
