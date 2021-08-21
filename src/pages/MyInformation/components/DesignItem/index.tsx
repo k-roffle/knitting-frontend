485cea1c093065595f31e29f947d37ff2017c006
Yuurilee <yurilee9696@gmail.com>
2021-08-21 18:45:25 +0900

    Feat: isLoading일 때 Skeleton 뜨도록 수정

--- src/pages/MyInformation/components/DesignItem/index.tsx
+++ src/pages/MyInformation/components/DesignItem/index.tsx
@@ -1,20 +1,37 @@
 import { Typography, ListItem } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import { Ellipsis } from 'components';
 import styled from 'styled-components';
 import { theme } from 'themes';
 import { palette } from 'themes/palette';
 
 interface Props {
-  name: string;
-  imageUrl?: string;
-  yarn: string;
-  tags: string[];
+  name?: string;
+  yarn?: string;
+  cover_image_url?: string;
+  tags?: string[];
   showDivider?: boolean;
+  isLoading?: boolean;
 }
 
 const StyledListItem = styled(ListItem)`
   padding: ${theme.spacing(3)};
   display: block;
+`;
+
+const ListItemContainer = styled.div`
+  display: flex;
+`;
+
+const ImageWrapper = styled.div`
+  width: ${theme.spacing(32)};
+  height: ${theme.spacing(17)};
+  margin-right: ${theme.spacing(2)};
+  overflow: hidden;
+`;
+
+const Content = styled.div`
+  width: 100%;
 `;
 
 const Name = styled(Typography)`
@@ -36,7 +53,15 @@ const Information = styled(Typography)`
   margin-bottom: ${theme.spacing(1)};
 `;
 
-const ThumbNail = styled.img``;
+const ThumbNail = styled.img`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: auto;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+`;
 
 const Divider = styled.div`
   margin: ${theme.spacing(3, -3, -3)};
@@ -45,24 +70,42 @@ const Divider = styled.div`
 `;
 
 const DesignItem = ({
-  name,
-  imageUrl,
-  yarn,
-  tags,
+  name = '',
+  cover_image_url: coverImageUrl,
+  yarn = '',
+  tags = [],
   showDivider = true,
+  isLoading = false,
 }: Props): React.ReactElement => {
   return (
     <StyledListItem button>
-      <div>
-        <Name variant="h4">
-          <Ellipsis text={name} />
-        </Name>
-        <Information variant="subtitle2">{yarn}</Information>
-        {tags.map((tag) => (
-          <DesignType key={tag}>{tag}</DesignType>
-        ))}
-      </div>
-      {imageUrl && <ThumbNail src={imageUrl} />}
+      <ListItemContainer>
+        {(isLoading || coverImageUrl) && (
+          <ImageWrapper>
+            {isLoading && (
+              <Skeleton variant="rect" width="100%" height="100%" />
+            )}
+            {coverImageUrl && <ThumbNail src={coverImageUrl} />}
+          </ImageWrapper>
+        )}
+        <Content>
+          <Name variant="h4">
+            {isLoading ? <Skeleton variant="text" /> : <Ellipsis text={name} />}
+          </Name>
+          <Information variant="subtitle2">
+            {isLoading ? <Skeleton variant="text" /> : yarn}
+          </Information>
+          {tags.map((tag) => (
+            <DesignType key={tag}>
+              {isLoading ? (
+                <Skeleton variant="text" width={theme.spacing(5)} />
+              ) : (
+                tag
+              )}
+            </DesignType>
+          ))}
+        </Content>
+      </ListItemContainer>
       {showDivider && <Divider />}
     </StyledListItem>
   );
