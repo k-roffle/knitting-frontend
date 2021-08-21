3b42dd61a47e257922a9f3040cd9c1d4b69ebf12
Yuurilee <yurilee9696@gmail.com>
2021-08-21 23:15:43 +0900

    Feat: useSWR 대신 useSWRInfinite 사용해서 더 불러오도록 수정

--- src/pages/MyInformation/hooks/useGetMyDesigns.ts
+++ src/pages/MyInformation/hooks/useGetMyDesigns.ts
@@ -1,4 +1,4 @@
-import useSWR, { SWRResponse } from 'swr';
+import { SWRInfiniteResponse, useSWRInfinite } from 'swr';
 import { getAccessToken } from 'utils/auth';
 import { DEFAULT_LIST_LENGTH, ListResponse } from 'utils/requestType';
 import { request } from 'utils/requests';
@@ -11,17 +11,37 @@ const getMyDesigns = async (pathname: string): Promise<VendorQueryResult> => {
   const { data } = await request({
     pathname,
     method: 'get',
-    params: {
-      count: DEFAULT_LIST_LENGTH,
-    },
     accessToken: getAccessToken(),
   });
 
   return data;
 };
 
-export const useGetMyDesigns = (): SWRResponse<VendorQueryResult, unknown> => {
-  const response = useSWR('designs/my', (pathname) => getMyDesigns(pathname));
+export const useGetMyDesigns = (): SWRInfiniteResponse<
+  VendorQueryResult,
+  ListResponse<DesignItemResponse>
+> => {
+  function getKey(
+    pageIndex: number,
+    previousPageData: VendorQueryResult | null,
+  ) {
+    const isLastCursor = previousPageData && !previousPageData.payload;
+
+    if (isLastCursor) {
+      return null;
+    }
+
+    const isFirstCursor = pageIndex === 0;
+    const lastCursor = previousPageData?.meta?.last_cursor;
+    const afterValue =
+      isFirstCursor || lastCursor == null ? `` : `&after=${lastCursor}`;
+
+    return `designs/my?count=${DEFAULT_LIST_LENGTH}${afterValue}`;
+  }
+
+  const response = useSWRInfinite(getKey, (pathname: string) =>
+    getMyDesigns(pathname),
+  );
 
   return response;
 };
