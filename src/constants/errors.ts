ef0c35bf9a1dd0cb24e0f5bfe92074f259f682da
Yuurilee <yurilee9696@gmail.com>
2021-09-18 17:36:46 +0900

    Feat: 제널러하게 쓰이는 에러 메시지 추가

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,3 +1,6 @@
+export const GENERAL_ERROR = '서버 요청에 실패했습니다.';
+export const NETWORK_ERROR = '네트워크 연결을 확인해주세요.';
+
 export const FAILED_TO_SAVE_DESIGN = '도안 저장에 실패했습니다.';
 export const FAILED_TO_PASTE_MORE_THAN_MAXIMUM_LENGTH =
   '최대 글자 수를 초과해 붙여넣기에 실패했습니다.';
