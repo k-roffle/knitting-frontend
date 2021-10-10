92fb0a38a2a5e255da596da6ebedc2424ccaefbc
Yuurilee <yurilee9696@gmail.com>
2021-10-11 03:52:57 +0900

    Feat: useFirebaseImageStorage - 이미지 여러개 업로드시 결과값 배열로 받도록 수정

--- src/hooks/useFirebaseImageStorage.ts
+++ src/hooks/useFirebaseImageStorage.ts
@@ -13,72 +13,142 @@ import {
 } from 'firebase/storage';
 import firebaseInit from 'firebaseInit';
 import decodeJwtToken from 'jwt-decode';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getAccessToken, TokenPayload } from 'utils/auth';
 
 type UploadStorage = {
+  localFileId: string;
   progress?: number;
   error?: StorageError;
   url?: string;
 };
 
-type FirebaseImageStorage = Omit<UploadStorage, 'error'> & {
-  uploadFile?(fileInformation: ImageInformation): void;
+type FirebaseImageStorage = {
+  uploadResults: UploadStorage[];
+  uploadFile?(): void;
 };
 
-const useFirebaseImageStorage = (path: string): FirebaseImageStorage => {
+interface Props {
+  path: string;
+  fileInformationList: ImageInformation[];
+}
+
+const getUpdatedUploadResult = (
+  localFileId: string,
+  uploadResult: UploadStorage,
+  updatedResult: UploadStorage,
+): UploadStorage => {
+  return uploadResult.localFileId === localFileId
+    ? updatedResult
+    : uploadResult;
+};
+
+const useFirebaseImageStorage = ({
+  path,
+  fileInformationList,
+}: Props): FirebaseImageStorage => {
   const token = getAccessToken();
 
   if (token == null) {
-    return {};
+    return {
+      uploadResults: [],
+    };
   }
 
-  const [uploadResult, setUploadResult] = useState<UploadStorage>({
-    progress: 0,
-  });
-
-  const { progress, error, url } = uploadResult ?? {};
+  const [uploadResults, setUploadResults] = useState<UploadStorage[]>([]);
+  const [showErrorToast, setShowErrorToast] = useState(false);
 
   useCommonSnackbar({
     message: FAILED_TO_UPLOAD_IMAGE,
     severity: 'error',
-    dependencies: [error],
+    dependencies: [showErrorToast],
   });
 
   const storage = getStorage(firebaseInit());
   const { id: userId }: TokenPayload = decodeJwtToken(token);
 
-  const onLoading = ({ bytesTransferred, totalBytes }: UploadTaskSnapshot) => {
-    const percentage = Math.floor((bytesTransferred / totalBytes) * 100);
-
-    setUploadResult({ ...uploadResult, progress: percentage });
-  };
-
-  const onError = (uploadError: StorageError) => {
-    setUploadResult({ ...uploadResult, error: uploadError });
-  };
-
-  const onComplete = (uploadTask: UploadTask) => (): void => {
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
-      setUploadResult({ ...uploadResult, url: downloadURL }),
+  const onLoading = (
+    { bytesTransferred, totalBytes }: UploadTaskSnapshot,
+    localFileId: string,
+  ) => {
+    const progress = Math.floor((bytesTransferred / totalBytes) * 100);
+    const updatedUploadResults = uploadResults.map((uploadResult) =>
+      getUpdatedUploadResult(localFileId, uploadResult, {
+        progress,
+        ...uploadResult,
+      }),
     );
+
+    setUploadResults(updatedUploadResults);
   };
 
-  const uploadFile = async (
-    fileInformation: ImageInformation,
-  ): Promise<void> => {
-    const { metadata, file } = fileInformation;
-    const extension = metadata.type.split('/')[1];
-    const storageRef = ref(
-      storage,
-      `${path}/${userId}/${Date.now()}.${extension}`,
+  const onError = (uploadError: StorageError, localFileId: string) => {
+    const updatedUploadResults = uploadResults.map((uploadResult) =>
+      getUpdatedUploadResult(localFileId, uploadResult, {
+        error: uploadError,
+        ...uploadResult,
+      }),
     );
-    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed', onLoading, onError, onComplete(uploadTask));
+    setUploadResults(updatedUploadResults);
   };
 
-  return { progress, url, uploadFile };
+  const onComplete = (uploadTask: UploadTask, localFileId: string): void => {
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      const updatedUploadResults = uploadResults.map((uploadResult) =>
+        getUpdatedUploadResult(localFileId, uploadResult, {
+          url: downloadURL,
+          ...uploadResult,
+        }),
+      );
+
+      setUploadResults(updatedUploadResults);
+    });
+  };
+
+  const uploadFile = async (): Promise<void> => {
+    fileInformationList.forEach((fileInformation) => {
+      const { metadata, file, url } = fileInformation;
+      const extension = metadata.type.split('/')[1];
+      const storageRef = ref(
+        storage,
+        `${path}/${userId}/${Date.now()}.${extension}`,
+      );
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      setUploadResults(
+        uploadResults.concat({
+          localFileId: url,
+        }),
+      );
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => onLoading(snapshot, url),
+        (error) => onError(error, url),
+        () => onComplete(uploadTask, url),
+      );
+    });
+  };
+
+  useEffect(() => {
+    const hasError = uploadResults.some(({ error }) => error);
+
+    setShowErrorToast(hasError);
+  }, [uploadResults]);
+
+  useEffect(() => {
+    const initialUploadResult = fileInformationList.map(
+      ({ url }): UploadStorage => {
+        return {
+          localFileId: url,
+        };
+      },
+    );
+
+    setUploadResults(initialUploadResult);
+  }, [fileInformationList]);
+
+  return { uploadResults, uploadFile };
 };
 
 export default useFirebaseImageStorage;
