340d78d729571e255530a3322271ac49d75009dd
jigeol <jiyaaany@cookat.co>
2021-10-20 10:25:44 +0900

    remove: 컴포넌트 props 사용 및 선언했던 props 제거

--- src/dumbs/InlineInput/index.tsx
+++ src/dumbs/InlineInput/index.tsx
@@ -1,22 +1,17 @@
-import { Grid, Input as BaseInput, Typography } from '@material-ui/core';
-import { InputBaseComponentProps } from '@material-ui/core/InputBase';
+import {
+  Grid,
+  Input as BaseInput,
+  InputProps,
+  Typography,
+} from '@material-ui/core';
 import { Variant } from '@material-ui/core/styles/createTypography';
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { theme } from 'themes';
 
-interface Props {
-  id: string;
-  type?: string;
+interface InlineInputProps extends InputProps {
   label: string;
   variant?: Variant | 'inherit';
-  value?: string | number;
-  placeholder?: string;
-  endAdornment?: React.ReactNode;
-  inputProps?: InputBaseComponentProps;
-  onChange: (
-    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
-  ) => void;
 }
 
 export const FormLabel = styled(Typography)`
@@ -39,32 +34,14 @@ const Input = styled(BaseInput)<{ type?: string }>`
     `}
 `;
 
-const InlineInput = ({
-  id,
-  type,
-  label,
-  variant,
-  value,
-  placeholder,
-  endAdornment,
-  onChange,
-}: Props): React.ReactElement => {
+const InlineInput = (props: InlineInputProps): React.ReactElement => {
   return (
     <InputGrid container item alignItems="center">
       <Grid item>
-        <FormLabel variant={variant}>{label}</FormLabel>
+        <FormLabel variant={props.variant}>{props.label}</FormLabel>
       </Grid>
       <Grid item>
-        <Input
-          id={id}
-          type={type}
-          aria-describedby={id}
-          placeholder={placeholder}
-          endAdornment={endAdornment}
-          value={value}
-          onChange={onChange}
-          required
-        />
+        <Input {...props} aria-describedby={props.id} required />
       </Grid>
     </InputGrid>
   );
