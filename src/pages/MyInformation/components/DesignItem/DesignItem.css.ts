2f65b94b9f69e3456c1a0c07a20b005efd9baaf6
Yuurilee <yurilee9696@gmail.com>
2021-10-29 23:44:26 +0900

    Refactor: DesignItem 스타일 관련 코드들 분리

---
+++ src/pages/MyInformation/components/DesignItem/DesignItem.css.ts
@@ -0,0 +1,81 @@
+import { Typography, ListItem, Checkbox } from '@material-ui/core';
+import styled from 'styled-components';
+import { theme } from 'themes';
+import { palette } from 'themes/palette';
+
+export const StyledListItem = styled(ListItem)`
+  padding: ${theme.spacing(3)};
+  display: block;
+`;
+
+export const ListItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ImageWrapper = styled.div`
+  width: ${theme.spacing(32)};
+  height: ${theme.spacing(17)};
+  margin-right: ${theme.spacing(2)};
+  overflow: hidden;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+`;
+
+export const Name = styled(Typography)`
+  margin-bottom: ${theme.spacing(1.5)};
+`;
+
+export const DesignType = styled.span`
+  display: inline-block;
+  color: ${palette.grey[800]};
+  background-color: rgba(0, 0, 0, 0.06);
+  padding: ${theme.spacing(0.5, 1)};
+  border-radius: ${theme.spacing(0.5)};
+  font-size: 14px;
+  margin-right: ${theme.spacing(1)};
+`;
+
+export const Information = styled(Typography)`
+  color: ${palette.grey[800]};
+  margin-bottom: ${theme.spacing(0.5)};
+`;
+
+export const CreatedDate = styled(Typography)`
+  display: block;
+  color: ${palette.text.secondary};
+  margin-bottom: ${theme.spacing(2)};
+`;
+
+export const ThumbNail = styled.img`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: auto;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+`;
+
+export const Divider = styled.div`
+  margin: ${theme.spacing(3, -3, -3)};
+  height: 1px;
+  background-color: ${palette.grey[300]};
+`;
+
+export const StyledCheckBox = styled(Checkbox)`
+  width: ${theme.spacing(3.5)};
+  height: ${theme.spacing(3.5)};
+  padding-right: ${theme.spacing(2)};
+
+  &:hover {
+    background-color: transparent;
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
--- src/pages/MyInformation/components/DesignItem/index.tsx
+++ src/pages/MyInformation/components/DesignItem/index.tsx
@@ -1,12 +1,23 @@
-import { Typography, ListItem, Checkbox } from '@material-ui/core';
 import { Ellipsis } from 'components';
 import Skeleton from 'dumbs/Skeleton';
 import { DesignItemResponse } from 'pages/MyInformation/hooks/types';
 import { MouseEvent } from 'react';
-import styled from 'styled-components';
 import { theme } from 'themes';
-import { palette } from 'themes/palette';
 import { formatDate } from 'utils/format';
+
+import {
+  StyledListItem,
+  ListItemContainer,
+  ImageWrapper,
+  Content,
+  Name,
+  DesignType,
+  Information,
+  CreatedDate,
+  ThumbNail,
+  Divider,
+  StyledCheckBox,
+} from './DesignItem.css';
 
 interface Props {
   showDivider?: boolean;
@@ -15,83 +26,6 @@ interface Props {
   checked?: boolean;
   onClick: (event: MouseEvent<HTMLDivElement>) => void;
 }
-
-const StyledListItem = styled(ListItem)`
-  padding: ${theme.spacing(3)};
-  display: block;
-`;
-
-const ListItemContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const ImageWrapper = styled.div`
-  width: ${theme.spacing(32)};
-  height: ${theme.spacing(17)};
-  margin-right: ${theme.spacing(2)};
-  overflow: hidden;
-`;
-
-const Content = styled.div`
-  width: 100%;
-`;
-
-const Name = styled(Typography)`
-  margin-bottom: ${theme.spacing(1.5)};
-`;
-
-const DesignType = styled.span`
-  display: inline-block;
-  color: ${palette.grey[800]};
-  background-color: rgba(0, 0, 0, 0.06);
-  padding: ${theme.spacing(0.5, 1)};
-  border-radius: ${theme.spacing(0.5)};
-  font-size: 14px;
-  margin-right: ${theme.spacing(1)};
-`;
-
-const Information = styled(Typography)`
-  color: ${palette.grey[800]};
-  margin-bottom: ${theme.spacing(0.5)};
-`;
-
-const CreatedDate = styled(Typography)`
-  display: block;
-  color: ${palette.text.secondary};
-  margin-bottom: ${theme.spacing(2)};
-`;
-
-const ThumbNail = styled.img`
-  position: relative;
-  top: 50%;
-  left: 50%;
-  width: 100%;
-  height: auto;
-  transform: translate(-50%, -50%);
-  cursor: pointer;
-`;
-
-const Divider = styled.div`
-  margin: ${theme.spacing(3, -3, -3)};
-  height: 1px;
-  background-color: ${palette.grey[300]};
-`;
-
-const StyledCheckBox = styled(Checkbox)`
-  width: ${theme.spacing(3.5)};
-  height: ${theme.spacing(3.5)};
-  padding-right: ${theme.spacing(2)};
-
-  &:hover {
-    background-color: transparent;
-  }
-
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-`;
 
 const DesignItem = ({
   name = '',
