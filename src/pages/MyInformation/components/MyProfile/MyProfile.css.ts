acea6a1c3f79c26a82ac8ad6833660f7ab6c2f8d
jigeol <jiyaaany@cookat.co>
2021-11-16 14:32:32 +0900

    refactor: styled-components 파일 분리

---
+++ src/pages/MyInformation/components/MyProfile/MyProfile.css.ts
@@ -0,0 +1,64 @@
+import { Button, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+import { flexVerticalAlign } from '../../../../styles/constants';
+import { theme } from '../../../../themes';
+import { palette } from '../../../../themes/palette';
+
+export const MyProfileContainer = styled.section`
+  display: inline-block;
+  width: 100%;
+  margin-bottom: ${theme.spacing(6)};
+`;
+
+export const ProfileContainer = styled.div`
+  ${flexVerticalAlign};
+
+  display: flex;
+  float: left;
+  margin-right: ${theme.spacing(3)};
+`;
+
+export const EmptyProfile = styled.span`
+  display: inline-block;
+  min-width: ${theme.spacing(10)};
+  min-height: ${theme.spacing(10)};
+  border-radius: ${theme.spacing(5)};
+  margin-right: ${theme.spacing(3)};
+  background-color: ${palette.grey[300]};
+`;
+
+export const Profile = styled.img`
+  display: inline-block;
+  min-width: ${theme.spacing(10)};
+  min-height: ${theme.spacing(10)};
+  border-radius: ${theme.spacing(5)};
+  margin-right: ${theme.spacing(3)};
+`;
+
+export const Name = styled(Typography)`
+  display: inline-block;
+  margin-right: ${theme.spacing(1)};
+`;
+
+export const Email = styled.span`
+  color: ${palette.text.secondary};
+`;
+
+export const MySalesSummary = styled.div`
+  display: flex;
+  margin-top: ${theme.spacing(1.5)};
+
+  > div:first-child {
+    margin-right: ${theme.spacing(3)};
+  }
+`;
+
+export const SalesSummaryCount = styled(Typography)`
+  text-align: center;
+`;
+
+export const CreateButton = styled(Button)`
+  float: right;
+  margin-top: ${theme.spacing((10 - 4.5) / 2)};
+`;
