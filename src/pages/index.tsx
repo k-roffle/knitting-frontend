ff221ad860370467c5cee8fd443ae385a260e6b3
Yuurilee <yurilee9696@gmail.com>
2021-11-19 00:53:09 +0900

    Delete: 사용하지 않는 CreateDesign/Detail 컴포넌트 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import Detail from './CreateDesign/Detail';
 import Pattern from './CreateDesign/Pattern';
 import Review from './CreateDesign/Review';
 import Error404 from './Error404';
 import Login from './Login';
 import LoginRedirected from './LoginRedirected';
 
-export { Detail, Login, Pattern, Review, Error404, LoginRedirected };
+export { Login, Pattern, Review, Error404, LoginRedirected };
