818acf511b75ac64d70aa52162810d17ced11a30
Yuurilee <yurilee9696@gmail.com>
2021-11-19 23:12:50 +0900

    Refactor: createDesign 폴더에 있는 컴포넌트 css 파일로 분리

---
+++ src/pages/CreateDesign/Review/Review.css.ts
@@ -0,0 +1,20 @@
+import { Grid, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Title = styled(Typography)`
+  font-weight: 600;
+`;
+
+export const Row = styled(Grid)`
+  padding: 12px;
+`;
+
+export const Label = styled(Typography)`
+  padding-bottom: 10px;
+  width: 100%;
+`;
+
+export const Contents = styled(Typography)`
+  white-space: pre-line;
+  padding: 5px;
+`;
