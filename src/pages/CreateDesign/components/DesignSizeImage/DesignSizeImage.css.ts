94a6f8c2d345c93a0a22280162200cc75e1b4150
Yuurilee <yurilee9696@gmail.com>
2021-11-19 23:12:50 +0900

    Refactor: createDesign 폴더에 있는 컴포넌트 css 파일로 분리

---
+++ src/pages/CreateDesign/components/DesignSizeImage/DesignSizeImage.css.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const DesignImageWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-width: 100%;
+    max-height: 450px;
+    margin-top: 8px;
+  }
+`;
---
+++ src/pages/CreateDesign/components/StepProgressBar/StepProgressBar.css.ts
@@ -0,0 +1,39 @@
+import StepLabel from '@material-ui/core/StepLabel';
+import styled, { css } from 'styled-components';
+import { palette } from 'themes/palette';
+import { zIndex } from 'themes/zIndex';
+
+export interface StepIconProps {
+  active: boolean;
+  completed: boolean;
+}
+
+export const StepIcon = styled.div<StepIconProps>`
+  color: ${palette.grey[200]};
+  display: flex;
+  height: 22px;
+  z-index: ${zIndex.mobileStepper};
+  align-items: center;
+  ${(props) =>
+    props.active &&
+    css`
+      color: ${palette.primary.main};
+    `};
+  div {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: currentColor;
+
+    ${(props) =>
+      props.completed &&
+      css`
+        color: ${palette.primary.main};
+        font-size: 18px;
+      `};
+  }
+`;
+
+export const StyledStepLabel = styled(StepLabel)`
+  word-break: keep-all;
+`;
