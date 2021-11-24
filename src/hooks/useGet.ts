d0cb8b4d0d23af713e4145ed72516e76371ecb98
jigeol <jiyaaany@cookat.co>
2021-11-24 11:26:03 +0900

    feat: 상품 상세 페이지 추가

--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -3,14 +3,19 @@ import { GENERAL_ERROR } from 'constants/errors';
 import { useCommonSnackbar } from 'components/CommonSnackbar/useCommonSnackbar';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { UseQueryResult } from 'react-query/types/react/types';
 import { RequestParam } from 'utils/requestType';
 import { getRequest } from 'utils/requests';
 
-export const useGet = ({
+export const useGet = <
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData
+>({
   pathname,
   errorMessage = GENERAL_ERROR,
   onSuccess,
-}: RequestParam) => {
+}: RequestParam): UseQueryResult<TData, TError> => {
   const [showError, setShowError] = useState<boolean>(false);
 
   useCommonSnackbar({
