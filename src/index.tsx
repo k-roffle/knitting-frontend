467023566b3b161e6684561f12f469f2a8f6cbe0
jiyaaany <jiyaaany@gmail.com>
2021-12-20 17:41:03 +0900

    fix: Yarn Berry import 이슈 픽스
    Signed-off-by: jiyaaany <jiyaaany@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import GlobalStyle from 'globalStyles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import GlobalStyle from './globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
