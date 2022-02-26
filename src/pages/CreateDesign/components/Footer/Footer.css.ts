cb218ea8385abb2fce884cb9b7af85e51edb0aa0
Yuurilee <yurilee9696@gmail.com>
2022-02-26 23:21:56 +0900

    refactor: nextLabel과 disabled 여부를 prop을 통해서 하도록

--- src/pages/CreateDesign/components/Footer/Footer.css.ts
+++ src/pages/CreateDesign/components/Footer/Footer.css.ts
@@ -4,4 +4,20 @@ export const FooterContainer = styled.div`
   display: flex;
   justify-content: space-between;
   padding: ${({ theme }) => theme.spacing(6, 0, 4)};
+  > button {
+    height: fit-content;
+  }
+`;
+
+export const NextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > button {
+    width: fit-content;
+  }
+  > span {
+    margin-top: ${({ theme }) => theme.spacing(1)};
+    color: ${({ theme }) => theme.palette.error.main};
+  }
 `;
