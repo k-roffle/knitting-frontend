9f553f32226d9906dacd4f1ec203c45998966322
jiyaaany <jiyaaany@gmail.com>
2022-03-13 22:27:56 +0900

    style: ThemeProvider 사용
    Signed-off-by: jiyaaany <jiyaaany@gmail.com>

--- src/pages/MyInformation/components/DesignItem/DesignItem.css.ts
+++ src/pages/MyInformation/components/DesignItem/DesignItem.css.ts
@@ -1,11 +1,11 @@
-import { theme } from 'knitting/themes';
 import { palette } from 'knitting/themes/palette';
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { ListItemButton, Typography, Checkbox } from '@mui/material';
 
 export const StyledListItemButton = styled(ListItemButton)`
-  padding: ${theme.spacing(3)};
+  padding: ${({ theme }) => theme.spacing(3)};
   display: block;
 `;
 
@@ -15,9 +15,12 @@ export const ListItemContainer = styled.div`
 `;
 
 export const ImageWrapper = styled.div`
-  width: ${theme.spacing(32)};
-  height: ${theme.spacing(17)};
-  margin-right: ${theme.spacing(2)};
+  ${({ theme }) => css`
+    width: ${theme.spacing(33.75)};
+    height: ${theme.spacing(25)};
+    border-radius: ${theme.spacing(1.25)};
+    margin-right: ${theme.spacing(2)};
+  `}
   overflow: hidden;
 `;
 
@@ -26,17 +29,19 @@ export const Content = styled.div`
 `;
 
 export const Name = styled(Typography)`
-  margin-bottom: ${theme.spacing(1.5)};
+  margin-bottom: ${({ theme }) => theme.spacing(1.5)};
 `;
 
 export const DesignType = styled.span`
   display: inline-block;
   color: ${palette.grey[800]};
   background-color: rgba(0, 0, 0, 0.06);
-  padding: ${theme.spacing(0.5, 1)};
-  border-radius: ${theme.spacing(0.5)};
+  ${({ theme }) => css`
+    padding: ${theme.spacing(0.5, 1)};
+    border-radius: ${theme.spacing(0.5)};
+    margin-right: ${theme.spacing(1)};
+  `};
   font-size: 14px;
-  margin-right: ${theme.spacing(1)};
 `;
 
 export const Price = styled.span`
@@ -48,13 +53,13 @@ export const Price = styled.span`
 
 export const Information = styled(Typography)`
   color: ${palette.grey[800]};
-  margin-bottom: ${theme.spacing(0.5)};
+  margin-bottom: ${({ theme }) => theme.spacing(0.5)};
 `;
 
 export const CreatedDate = styled(Typography)`
   display: block;
   color: ${palette.text.secondary};
-  margin-bottom: ${theme.spacing(2)};
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
 `;
 
 export const ThumbNail = styled.img`
@@ -68,15 +73,17 @@ export const ThumbNail = styled.img`
 `;
 
 export const Divider = styled.div`
-  margin: ${theme.spacing(3, -3, -3)};
+  margin: ${({ theme }) => theme.spacing(3, -3, -3)};
   height: 1px;
   background-color: ${palette.grey[300]};
 `;
 
 export const StyledCheckBox = styled(Checkbox)`
-  width: ${theme.spacing(3.5)};
-  height: ${theme.spacing(3.5)};
-  padding-right: ${theme.spacing(2)};
+  ${({ theme }) => css`
+    width: ${theme.spacing(3.5)};
+    height: ${theme.spacing(3.5)};
+    padding-right: ${theme.spacing(2)};
+  `};
 
   &:hover {
     background-color: transparent;
