f594cc3fe91403e018c725f662f250bc15d36e1c
jiyaaany <jiyaaany@gmail.com>
2022-09-15 10:42:42 +0100

    feat: 정의되는 라우터가 많아짐에 따라 Routers 파일로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import CommonSnackbar from 'knitting/components/CommonSnackbar';
-import {
-  MY_INFORMATION_ROUTER_ROOT,
-  LOGIN_ROUTER_ROOT,
-  ERROR_PATH,
-  ROUTER_PATH,
-} from 'knitting/constants/path';
-import { Error404 } from 'knitting/pages';
-import Login from 'knitting/routers/LoginRouter';
-import MyInformation from 'knitting/routers/MyInformationRouter';
 import { theme } from 'knitting/themes';
-import {
-  LoginRoute,
-  ProtectedRoute,
-  RouteWithoutTrailingSlash,
-} from 'knitting/utils/route';
+import { RouteWithoutTrailingSlash } from 'knitting/utils/route';
 
 import { Global } from '@emotion/react';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import GlobalStyle from './globalStyles';
+import Routers from './routers';
 
 const queryClient = new QueryClient();
 
@@ -35,34 +23,7 @@ const App = (): React.ReactElement => {
             <ThemeProvider theme={theme}>
               <Global styles={GlobalStyle} />
               <RouteWithoutTrailingSlash />
-              <Routes>
-                <Route
-                  index
-                  element={
-                    <ProtectedRoute>
-                      <MyInformation />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path={MY_INFORMATION_ROUTER_ROOT + ROUTER_PATH}
-                  element={
-                    <ProtectedRoute>
-                      <MyInformation />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path={LOGIN_ROUTER_ROOT + ROUTER_PATH}
-                  element={
-                    <LoginRoute>
-                      <Login />
-                    </LoginRoute>
-                  }
-                />
-                <Route path={ERROR_PATH} element={<Error404 />} caseSensitive />
-                <Route path="*" element={<Error404 />} />
-              </Routes>
+              <Routers />
             </ThemeProvider>
           </StyledEngineProvider>
           <CommonSnackbar />
---
+++ src/routers/index.tsx
@@ -0,0 +1,46 @@
+import { Route, Routes } from 'react-router-dom';
+
+import {
+  ERROR_PATH,
+  LOGIN_ROUTER_ROOT,
+  MY_INFORMATION_ROUTER_ROOT,
+  ROUTER_PATH,
+} from '../constants/path';
+import { Error404 } from '../pages';
+import { LoginRoute, ProtectedRoute } from '../utils/route';
+
+import Login from './LoginRouter';
+import MyInformation from './MyInformationRouter';
+
+const Routers = () => (
+  <Routes>
+    <Route
+      path={ROUTER_PATH}
+      element={
+        <ProtectedRoute>
+          <MyInformation />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path={MY_INFORMATION_ROUTER_ROOT + ROUTER_PATH}
+      element={
+        <ProtectedRoute>
+          <MyInformation />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path={LOGIN_ROUTER_ROOT + ROUTER_PATH}
+      element={
+        <LoginRoute>
+          <Login />
+        </LoginRoute>
+      }
+    />
+    <Route path={ERROR_PATH} element={<Error404 />} caseSensitive />
+    <Route path="*" element={<Error404 />} />
+  </Routes>
+);
+
+export default Routers;
